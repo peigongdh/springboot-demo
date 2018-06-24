7fa4c53e69f586945108cbb739c4329ea56ae16b
zhangpei-home <peigong.dh@qq.com>
2018-06-24 19:41:45 +0800

    ch03

---
+++ src/main/java/com/peigongdh/springbootdemo/ReadingListController.java
@@ -0,0 +1,43 @@
+package com.peigongdh.springbootdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+
+    private ReadingListRepository readingListRepository;
+
+    private AmazonProperties amazonProperties;
+
+    @Autowired
+    public ReadingListController(ReadingListRepository readingListRepository, AmazonProperties amazonProperties) {
+        this.readingListRepository = readingListRepository;
+        this.amazonProperties = amazonProperties;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String readersBooks(Reader reader, Model model) {
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonID", amazonProperties.getAssociateId());
+        }
+        return "readingList";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String addToReadingList(Reader reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/";
+    }
+
+}
